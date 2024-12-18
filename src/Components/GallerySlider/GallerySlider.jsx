7b453e08398b17d30e00b81acce604dafb8eb310
Paulo Galvão <paulogalvaoj21@gmail.com>
2024-12-18 20:30:10 -0300

    gallery: style, logic, slide, fullscreen

--- src/Components/GallerySlider/GallerySlider.jsx
+++ src/Components/GallerySlider/GallerySlider.jsx
@@ -2,13 +2,13 @@ import photos from "./gallery.js"
 import { useState } from "react";
 import "./gallery.style.css"
 
-export default function GallerySlider() {
-  
-    const [count, setCount] = useState(0);
-    const [name, setName] = useState(photos.uepg);
-    let list = name;
-    const classActive = document.querySelectorAll(".active");
 
+export default function GallerySlider() {
+    const [count, setCount] = useState(0);
+    const [galleryName, setName] = useState(photos.uepg);
+
+    // funcionamento da troca de imagens
+    
     function setNextImg() {
         function next(list) {           
             if(count == (list.length-1)) {
@@ -18,7 +18,7 @@ export default function GallerySlider() {
             }        
             
         }
-        next(list);
+        next(galleryName);
         
     }
     
@@ -30,76 +30,143 @@ export default function GallerySlider() {
                 setCount(count - 1);
             }
         }
-        last(list);        
+        last(galleryName);        
+    }
+
+    // funcionamento da tela fullscreen
+
+    function setFullScreen() {
+        // impede que a tela role enquanto a imagem está em tela cheia
+        document.body.classList.add("stop-scrolling");
+
+        // poe o slider em tela cheia
+        const imgContainer = document.querySelector(".img-container");
+        imgContainer.classList.add("full-screen");
+        
+        // muda a visibilidade dos botões de tela cheia
+        const fullScreenBtn = document.querySelector(".full-screen-btn");
+        fullScreenBtn.classList.remove("active");     
+        const exitFullScreenBtn = document.querySelector(".exit-full-screen-btn");
+        exitFullScreenBtn.classList.add("active");
+        
+        // retira o menu lateral da tela
+        const asideMenu = document.querySelector("aside");
+        asideMenu.classList.remove("active");
+
+        // coloca a imagem em tamanho real
+        const img = document.querySelector(".gallery-banner img");
+        img.classList.add("full-size");
+    }
+
+    function removeFullScreen() {
+        // permite que a tela continue rolando enquanto a imagem está em tela cheia
+        document.body.classList.remove("stop-scrolling");
+
+        // tira o container da imagem da tela cheia 
+        const imgContainer = document.querySelector(".img-container");
+        imgContainer.classList.remove("full-screen");
+
+        // torna o botao de tela cheia visivel
+        const fullScreenBtn = document.querySelector(".full-screen-btn");
+        fullScreenBtn.classList.add("active");
+        
+        // torna o botao de sair da tela cheia invisivel
+        const exitFullScreenBtn = document.querySelector(".exit-full-screen-btn");
+        exitFullScreenBtn.classList.remove("active");
+        
+        // retorna o menu lateral à tela
+        const asideMenu = document.querySelector("aside");
+        asideMenu.classList.add("active");
+        
+        // retorna a imagem ao tamanho padrão da tela
+        const img = document.querySelector(".gallery-banner img");
+        img.classList.remove("full-size");
+        
+    } 
+
+    // mudança do tipo de galeria atravez da navegação lateral
+
+    function setGalleryType( gallery, e ) {
+        // remove o efeito da classe active do item anterior
+        const classActive = document.querySelectorAll("ul li.active");
+        classActive[0].classList.remove("active");
+        e.target.classList.add("active"); 
+        
+        // seta o novo tipo de galeria
+        setName(gallery);
+        setCount(0);
+        
     }
 
     return (
         <div className="gallery-container">
-            <aside>
+            <aside className="active">
                 <ul>
-                    <li onClick={ (e) => {
-                        list = photos.uepg;
-                        setName(list);
-                        classActive.forEach( item => item.classList.remove("active"));
-                        e.target.classList.add("active"); 
+                    <li className="active" onClick={ (e) => {
+                        setGalleryType(photos.uepg, e);                        
                     }}>
                         Atividades em gabinete
                     </li>
-                    <li onClick={ (e) => {
-                        list = photos.fire;
-                        setName(list);
-                         classActive.forEach( item => item.classList.remove("active"));
-                        e.target.classList.add("active"); 
+                    <li onClick={ (e) => {  
+                        setGalleryType(photos.fire, e);                      
+                        
                     }}>
                         Curso de primeiros socorros
                     </li>
                     <li onClick={ (e) => {
-                        list = photos.maquete;
-                        setName(list);
-                         classActive.forEach( item => item.classList.remove("active"));
-                        e.target.classList.add("active"); 
+                        setGalleryType(photos.maquete, e); 
+                         
                     }}>
                         Curso de maquete
                     </li>
                     <li onClick={ (e) => {
-                        list = photos.itaiacoca;
-                        setName(list);
-                         classActive.forEach( item => item.classList.remove("active"));
-                        e.target.classList.add("active"); 
+                        setGalleryType(photos.itaiacoca, e); 
+                         
                     }}>
                         Trabalhos de campo em Itaiacoca, Ponta Grossa-PR
                     </li>
                     <li onClick={ (e) => {
-                        list = photos.imbituva;
-                        setName(list);
-                         classActive.forEach( item => item.classList.remove("active"));
-                        e.target.classList.add("active"); 
+                        setGalleryType(photos.imbituva, e);
+                        
                     }}>
                         Trabalhos de campo em Imbituva-PR/Guamiranga-PR
                     </li>
                     <li onClick={ (e) => {
-                        list = photos.curso;
-                        setName(list);
-                        classActive.forEach( item => item.classList.remove("active"));
-                        e.target.classList.add("active"); 
+                        setGalleryType(photos.curso, e);
+                        
                     }}>
                         Curso de aperfeiçoamento em Solo e Água para os Professores da rede básica de ensino - Imbituva-PR
                     </li>
                     <li onClick={ (e) => {
-                        list = photos.mapas;
-                        setName(list);
-                        classActive.forEach( item => item.classList.remove("active"));
-                        e.target.classList.add("active"); 
+                        setGalleryType(photos.mapas, e);
+                        
                     }}>
                         Produções Cartográficas das áreas de estudo
                     </li>
                 </ul>
             </aside>
             <div className="gallery-banner">
-                <img style={{maxWidth: 600}} src={list[count].src} alt={list[count].description} />
-                <button onClick={setLastImg}>Back</button>
-                <button onClick={setNextImg}>Next</button>
+                <div className="img-container">
+                    <img src={galleryName[count].src} alt={galleryName[count].description} />
+                    <button className="back-btn" onClick={setLastImg}>
+                        <i className='bx bx-chevron-left'></i>
+                    </button>
+                    <button className="next-btn" onClick={setNextImg}>
+                        <i className='bx bx-chevron-right'></i>
+                    </button>
+                    <div className="counter-img">
+                        <span >{count+1} / </span>
+                        <span>{galleryName.length}</span>
+                    </div>
+                    <button onClick={removeFullScreen} className="exit-full-screen-btn">
+                        <i className='bx bx-exit-fullscreen'></i>
+                    </button>
+                    <button onClick={setFullScreen} className="full-screen-btn active">
+                        <i className="bx bx-fullscreen"></i>
+                    </button>
+                </div>
             </div>
+            
         </div>
     )
 }
--- src/Components/GallerySlider/gallery.js
+++ src/Components/GallerySlider/gallery.js
@@ -1,55 +1,55 @@
 const uepg = [
     {
         id: 1,
-        src: "../../../public/galery/gabinete/uepg-1.jpeg",
+        src: "/galery/gabinete/uepg-1.jpeg",
         type: "uepg",
         description: "Atividades em gabinete"
     },
     {
         id: 2,
-        src: "../../../public/galery/gabinete/uepg-2.jpeg",
+        src: "/galery/gabinete/uepg-2.jpeg",
         type: "uepg",
         description: "Atividades em gabinete"
     },
     {
         id: 3,
-        src: "../../../public/galery/gabinete/uepg-3.jpeg",
+        src: "/galery/gabinete/uepg-3.jpeg",
         type: "uepg",
         description: "Atividades em gabinete"
     },
     {
         id: 4,
-        src: "../../../public/galery/gabinete/uepg-4.jpeg",
+        src: "/galery/gabinete/uepg-4.jpeg",
         type: "uepg",
         description: "Atividades em gabinete"
     },
     {
         id: 5,
-        src: "../../../public/galery/gabinete/uepg-5.jpeg",
+        src: "/galery/gabinete/uepg-5.jpeg",
         type: "uepg",
         description: "Atividades em gabinete"
     },
     {
         id: 6,
-        src: "../../../public/galery/gabinete/uepg-6.jpeg",
+        src: "/galery/gabinete/uepg-6.jpeg",
         type: "uepg",
         description: "Atividades em gabinete"
     },
     {
         id: 7,
-        src: "../../../public/galery/gabinete/uepg-7.jpg",
+        src: "/galery/gabinete/uepg-7.jpg",
         type: "uepg",
         description: "Atividades em gabinete"
     },
     {
         id: 8,
-        src: "../../../public/galery/gabinete/uepg-8.jpg",
+        src: "/galery/gabinete/uepg-8.jpg",
         type: "uepg",
         description: "Atividades em gabinete"
     },
     {
         id: 9,
-        src: "../../../public/galery/gabinete/uepg-9.jpg",
+        src: "/galery/gabinete/uepg-9.jpg",
         type: "uepg",
         description: "Atividades em gabinete"
     }
@@ -58,19 +58,19 @@ const uepg = [
 const fire = [
     {
         id: 1,
-        src: "../../../public/galery/fire/bber-1.jpg",
+        src: "/galery/fire/bber-1.jpg",
         type: "fire",
         description: "Curso de primeiros socorros"
     },
     {
         id: 2,
-        src: "../../../public/galery/fire/bber-2.jpg",
+        src: "/galery/fire/bber-2.jpg",
         type: "fire",
         description: "Curso de primeiros socorros"
     },
     {
         id: 3,
-        src: "../../../public/galery/fire/bber-3.jpg",
+        src: "/galery/fire/bber-3.jpg",
         type: "fire",
         description: "Curso de primeiros socorros"
     }
@@ -79,19 +79,19 @@ const fire = [
 const maquete = [
     {
         id: 1,
-        src: "../../../public/galery/maquete/maq-01.jpeg",
+        src: "/galery/maquete/maq-01.jpeg",
         type: "maq",
         description: "Curso de maquete"
     },
     {
         id: 2,
-        src: "../../../public/galery/maquete/maq-02.jpeg",
+        src: "/galery/maquete/maq-02.jpeg",
         type: "maq",
         description: "Curso de maquete"
     },
     {
         id: 3,
-        src: "../../../public/galery/maquete/maq-03.jpeg",
+        src: "/galery/maquete/maq-03.jpeg",
         type: "maq",
         description: "Curso de maquete"
     }
@@ -100,73 +100,73 @@ const maquete = [
 const itaiacoca = [
     {
         id: 1,
-        src: "../../../public/galery/itaiacoca/ita-1.jpeg",
+        src: "/galery/itaiacoca/ita-1.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 2,
-        src: "../../../public/galery/itaiacoca/ita-2.jpeg",
+        src: "/galery/itaiacoca/ita-2.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 3,
-        src: "../../../public/galery/itaiacoca/ita-3.jpeg",
+        src: "/galery/itaiacoca/ita-3.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 4,
-        src: "../../../public/galery/itaiacoca/ita-4.jpeg",
+        src: "/galery/itaiacoca/ita-4.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 5,
-        src: "../../../public/galery/itaiacoca/ita-5.jpeg",
+        src: "/galery/itaiacoca/ita-5.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 6,
-        src: "../../../public/galery/itaiacoca/ita-6.jpeg",
+        src: "/galery/itaiacoca/ita-6.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 7,
-        src: "../../../public/galery/itaiacoca/ita-7.jpeg",
+        src: "/galery/itaiacoca/ita-7.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 8,
-        src: "../../../public/galery/itaiacoca/ita-8.jpeg",
+        src: "/galery/itaiacoca/ita-8.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 9,
-        src: "../../../public/galery/itaiacoca/ita-9.jpeg",
+        src: "/galery/itaiacoca/ita-9.jpeg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 10,
-        src: "../../../public/galery/itaiacoca/ita-10.jpg",
+        src: "/galery/itaiacoca/ita-10.jpg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 11,
-        src: "../../../public/galery/itaiacoca/ita-11.jpg",
+        src: "/galery/itaiacoca/ita-11.jpg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
     {
         id: 12,
-        src: "../../../public/galery/itaiacoca/ita-12.jpg",
+        src: "/galery/itaiacoca/ita-12.jpg",
         type: "ita",
         description: "Curso de primeiros socorros"
     },
@@ -176,55 +176,55 @@ const itaiacoca = [
 const imbituva = [
     {
         id: 1,
-        src: "../../../public/galery/imbituva/imb-1.jpg",
+        src: "/galery/imbituva/imb-1.jpg",
         type: "imb",
         description: "Curso de primeiros socorros"
     },
     {
         id: 2,
-        src: "../../../public/galery/imbituva/imb-2.jpg",
+        src: "/galery/imbituva/imb-2.jpg",
         type: "imb",
         description: "Curso de primeiros socorros"
     },
     {
         id: 3,
-        src: "../../../public/galery/imbituva/imb-3.jpg",
+        src: "/galery/imbituva/imb-3.jpg",
         type: "imb",
         description: "Curso de primeiros socorros"
     },
     {
         id: 4,
-        src: "../../../public/galery/imbituva/imb-4.jpg",
+        src: "/galery/imbituva/imb-4.jpg",
         type: "imb",
         description: "Curso de primeiros socorros"
     },
     {
         id: 5,
-        src: "../../../public/galery/imbituva/imb-5.jpg",
+        src: "/galery/imbituva/imb-5.jpg",
         type: "imb",
         description: "Curso de primeiros socorros"
     },
     {
         id: 6,
-        src: "../../../public/galery/imbituva/imb-6.jpg",
+        src: "/galery/imbituva/imb-6.jpg",
         type: "imb",
         description: "Curso de primeiros socorros"
     },
     {
         id: 7,
-        src: "../../../public/galery/imbituva/imb-7.jpg",
+        src: "/galery/imbituva/imb-7.jpg",
         type: "imb",
         description: "Curso de primeiros socorros"
     },
     {
         id: 8,
-        src: "../../../public/galery/imbituva/imb-8.jpg",
+        src: "/galery/imbituva/imb-8.jpg",
         type: "imb",
         description: "Curso de primeiros socorros"
     },
     {
         id: 9,
-        src: "../../../public/galery/imbituva/imb-9.jpeg",
+        src: "/galery/imbituva/imb-9.jpeg",
         type: "imb",
         description: "Curso de primeiros socorros"
     }
@@ -234,25 +234,25 @@ const imbituva = [
 const curso = [
     {
         id: 1,
-        src: "../../../public/galery/curso/curso-1.jpeg",
+        src: "/galery/curso/curso-1.jpeg",
         type: "curso",
         description: "Curso de primeiros socorros"
     },
     {
         id: 2,
-        src: "../../../public/galery/curso/curso-2.jpeg",
+        src: "/galery/curso/curso-2.jpeg",
         type: "curso",
         description: "Curso de primeiros socorros"
     },
     {
         id: 3,
-        src: "../../../public/galery/curso/curso-3.jpeg",
+        src: "/galery/curso/curso-3.jpeg",
         type: "curso",
         description: "Curso de primeiros socorros"
     },
     {
         id: 4,
-        src: "../../../public/galery/curso/curso-4.jpeg",
+        src: "/galery/curso/curso-4.jpeg",
         type: "curso",
         description: "Curso de primeiros socorros"
     }
@@ -261,55 +261,55 @@ const curso = [
 const mapas = [
     {
         id: 1,
-        src: "../../../public/galery/mapas/mapa-1.png",
+        src: "/galery/mapas/mapa-1.png",
         type: "mapa",
         description: "Curso de primeiros socorros"
     },
     {
         id: 2,
-        src: "../../../public/galery/mapas/mapa-2.png",
+        src: "/galery/mapas/mapa-2.png",
         type: "mapa",
         description: "Curso de primeiros socorros"
     },
     {
         id: 3,
-        src: "../../../public/galery/mapas/mapa-3.png",
+        src: "/galery/mapas/mapa-3.png",
         type: "mapa",
         description: "Curso de primeiros socorros"
     },
     {
         id: 4,
-        src: "../../../public/galery/mapas/mapa-4.png",
+        src: "/galery/mapas/mapa-4.png",
         type: "mapa",
         description: "Curso de primeiros socorros"
     },
     {
         id: 5,
-        src: "../../../public/galery/mapas/mapa-5.png",
+        src: "/galery/mapas/mapa-5.png",
         type: "mapa",
         description: "Curso de primeiros socorros"
     },
     {
         id: 6,
-        src: "../../../public/galery/mapas/mapa-6.png",
+        src: "/galery/mapas/mapa-6.png",
         type: "mapa",
         description: "Curso de primeiros socorros"
     },
     {
         id: 7,
-        src: "../../../public/galery/mapas/mapa-7.png",
+        src: "/galery/mapas/mapa-7.png",
         type: "mapa",
         description: "Curso de primeiros socorros"
     },
     {
         id: 8,
-        src: "../../../public/galery/mapas/mapa-8.png",
+        src: "/galery/mapas/mapa-8.png",
         type: "mapa",
         description: "Curso de primeiros socorros"
     }
 ]
 
-export default {
+export default {    
     uepg,
     fire,
     maquete,
